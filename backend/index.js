46bbfd525266098c3f462673abd94c21273df06b
aldairandre99 <nzolageral@gmail.com>
2026-06-28 14:37:30 +0100

    Update CORS configuration to use environment variable for frontend URL

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: "https://frontend-6j0r.onrender.com"
+  origin: process.env.FRONTEND_URL || "http://localhost:5173/"
 }));
 app.use(express.json());
 
